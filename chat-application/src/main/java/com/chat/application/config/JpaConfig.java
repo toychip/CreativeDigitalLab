b02365719c64d2a7853e5b8f688d0323b0c0f557
toychip <dlawnsgud427@naver.com>
2026-05-27 22:28:34 +0900

    feat: read model 도입을 위한 JPA 스캔 영역 + 비동기 활성화 config 분리 (#9)

--- chat-application/src/main/java/com/chat/application/config/JpaConfig.java
+++ chat-application/src/main/java/com/chat/application/config/JpaConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EntityScan(basePackages = "com.chat.application.event")
-@EnableJpaRepositories(basePackages = "com.chat.application.event")
+@EntityScan(basePackages = "com.chat.application")
+@EnableJpaRepositories(basePackages = "com.chat.application")
 public class JpaConfig {
 }
